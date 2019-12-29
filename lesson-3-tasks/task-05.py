e52991560815ecb895b0a8e4520763375f81a432
Alexander Babkin <babkin.a@tecomgroup.ru>
2019-12-29 03:49:02 +0300

    added another implementation for task-03 and used sum for task-05

--- lesson-3-tasks/task-05.py
+++ lesson-3-tasks/task-05.py
@@ -18,8 +18,7 @@ is_end = False
 while not is_end:
     user_input = input('Введите числа разелленые пробелом, символ \'q\' является завершающим: ')
     num_list, is_end = parse_user_input(user_input)
-    for num in num_list:
-        num_sum += num
+    num_sum += sum(num_list)
     if is_end:
         print(f'Финальная сумма равна: {num_sum}')
     else:
